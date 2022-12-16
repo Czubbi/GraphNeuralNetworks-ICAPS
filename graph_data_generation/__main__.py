0f1211b1b78518db295b6c5a66987ec4a93a73a7
Bartosz Lachowicz <jendrekpb@gmail.com>
2022-12-16 16:16:21 +0100

    Implement graph data generation for big dataset

--- graph_data_generation/__main__.py
+++ graph_data_generation/__main__.py
@@ -9,11 +9,27 @@ if __name__ == "__main__":
 
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
-        "--data_folder",
+        "domains_dir",
         help="path to folder with domains",
-        default="downward-main/useful-actions-dataset-main",
+        # default="downward-main/useful-actions-dataset-main",
     )
-
+    argparser.add_argument("graph_type", help="cg or pdg")
+    argparser.add_argument("output_dir", help="output folder")
+    argparser.add_argument("--need_sas", help="need sas file")
+    argparser.add_argument("--from_usefull", help="useful_actions_dataset")
     options = argparser.parse_args()
-    data_folder = options.data_folder
-    get_domain_folders(data_folder)
+    domains_dir = options.domains_dir
+    graph_type = options.graph_type
+    output_dir = options.output_dir
+    need_sas = bool(options.need_sas)
+    from_usefull = bool(options.from_usefull)
+
+    print(f"domains_dir: {domains_dir}")
+    print(f"graph_type: {graph_type}")
+    print(f"output_dir: {output_dir}")
+    print(f"need_sas: {need_sas}")
+    print(f"from_usefull: {from_usefull}")
+
+    get_domain_folders(domains_dir=domains_dir, graph_type=graph_type, output_dir=output_dir, need_sas=need_sas, from_usefull=from_usefull)
+
+# python -m graph_data_generation solved_instances big_dataset
