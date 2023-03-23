383789fe8fe6ba0524d8e4ad8c3996a3b320088d
Bartosz Lachowicz <jendrekpb@gmail.com>
2023-03-23 14:52:53 +0100

    Fixed testing function

--- test.bash
+++ test.bash
@@ -1,5 +1,24 @@
-./scorpion/fast-downward.py --transform-task-options retry-with,h2 --transform-task src/preprocessor.command zztest/p151_2_2_4_5_1/p151_2_2_4_5_1.pddl --search "astar(scp_online([
+run_good() {
+./scorpion/fast-downward.py --transform-task-options retry-with,h2 --transform-task src/preprocessor.command zztest/good.pddl --search "astar(scp_online([
         projections(sys_scp(max_time=100, max_time_per_restart=10)),
         cartesian()],
         saturator=perimstar, max_time=1000, interval=10K, orders=greedy_orders()),
         pruning=limited_pruning(pruning=atom_centric_stubborn_sets(), min_required_pruning_ratio=0.2))"
+}
+
+run_bad() {
+./scorpion/fast-downward.py --transform-task-options retry-with,h2 --transform-task src/preprocessor.command zztest/bad.pddl --search "astar(scp_online([
+        projections(sys_scp(max_time=100, max_time_per_restart=10)),
+        cartesian()],
+        saturator=perimstar, max_time=1000, interval=10K, orders=greedy_orders()),
+        pruning=limited_pruning(pruning=atom_centric_stubborn_sets(), min_required_pruning_ratio=0.2))"
+}
+
+
+if [ $1 = "good" ]; then
+echo  running good
+run_good
+elif [ $1 = "bad" ]; then
+echo running bad
+run_bad
+fi
