6eb4bcb8d29a6927a97e6abd8d4cfdad4758c6e6
Bartosz Lachowicz <jendrekpb@gmail.com>
2023-05-15 23:11:34 +0200

    Remove causal code part 1

--- src/graph_building/exceptions.py
+++
@@ -1,3 +0,0 @@
-class EmptyCausalGraphError(Exception):
-    def __init__(self, sasfile_path: str, good_operators_path: str):
-        self.message = f"Empty causal graph for {sasfile_path} and {good_operators_path}"
