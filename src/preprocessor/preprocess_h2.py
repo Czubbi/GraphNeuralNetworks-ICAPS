6f1eb164bf0e08081a2a2417bf1e45f35799d52c
KantiCodes <jendrekpb@gmail.com>
2023-05-31 11:02:09 +0200

    Added time limit to preprocessor.h2

--- src/preprocessor/preprocess_h2.py
+++ src/preprocessor/preprocess_h2.py
@@ -7,5 +7,12 @@ def run_h2_preprocessor_on_file(path_to_file: str, time_limit,  **kwargs):
         kwargs = {'preexec_fn': None}
 
     with open(path_to_file, "r") as f:
-        code = subprocess.check_call(h2_path, stdin=f, timeout=time_limit, **kwargs)
-        print("Transformer returned with the code:", code)
+        # code = subprocess.check_call(h2_path, stdin=f, timeout=, **kwargs)
+        try:
+            proc = subprocess.Popen(h2_path, stdin=f, stderr=subprocess.PIPE, **kwargs)
+            res = proc.communicate(timeout=time_limit)
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            res = proc.communicate()
+            print("H2 is taking too long, let's kill it")
+        print("Transformer returned with the code:", res)
